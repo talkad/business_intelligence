4e2df38f2c0baad52ad841a070c4a01847ac9c67
talkad <talkad@post.bgu.ac.il>
2022-05-21 15:39:51 +0300

    loading dataset

--- R/DecisionTree.R
+++ R/DecisionTree.R
@@ -1,1 +1,15 @@
-print("hello world!")
+
+
+load_dataset <<- function(filepath) {
+  data = read.csv(filepath)
+  df = data.frame(data)
+  return(df)
+}
+
+
+
+filepath <- readline(prompt="Enter File Path: ")
+load_dataset(filepath)
+# C:/Users/tal74/projects/business_intelligence/R/Loan_dataset.csv
+
+
